22fc84db5d758712fcf34aa40ce6c74293e8f676
unknown <azitabhdinesh@gmail.com>
2022-05-23 11:13:03 +0530

    login and logout are added.

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthService } from '../service/auth.service';
 
 @Injectable({
@@ -15,7 +14,8 @@ export class AuthGuard implements CanActivate {
       return true;
     }
     else {
-      this.router.navigate(['/home']);
+      this.authService.setErroMsg("You are not authorized to access this page. Routing to Login page.")
+      this.router.navigate(['/login']);
     }
     return false;
   }
