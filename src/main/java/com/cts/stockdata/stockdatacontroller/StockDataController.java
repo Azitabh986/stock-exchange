c3d41713da7dd13623f97e07a921f4b5cd060e44
unknown <azitabhdinesh@gmail.com>
2022-07-28 18:14:22 +0530

    banner is added.

--- src/main/java/com/cts/stockdata/stockdatacontroller/StockDataController.java
+++ src/main/java/com/cts/stockdata/stockdatacontroller/StockDataController.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.cts.stockdata.model.CompanyDetails;
 
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @FeignClient(name = "STOCK-EXCHANGE")
 public interface StockDataController {
 	
