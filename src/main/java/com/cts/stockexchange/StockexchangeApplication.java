d4728983b64a37bde541814ce62dedf4de593c91
Azitabh986 <58431561+Azitabh986@users.noreply.github.com>
2022-08-18 18:18:29 +0530

    Update StockexchangeApplication.java

--- src/main/java/com/cts/stockexchange/StockexchangeApplication.java
+++ src/main/java/com/cts/stockexchange/StockexchangeApplication.java
@@ -3,7 +3,7 @@ package com.cts.stockexchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@EnableEurekaClient
+// @EnableEurekaClient
 @SpringBootApplication
 public class StockexchangeApplication {
 
